1e38c5292cba5e628c8be10c304fde3b8653e5b1
fidelincidane <fidelincidane@gmail.com>
2026-03-15 11:24:35 +0100

    Add migrate to wsgi

--- mantenimiento/wsgi.py
+++ mantenimiento/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mantenimiento.settings')
 
 application = get_wsgi_application()
+
+# Ejecutar migrate al inicio
+from django.core.management import execute_from_command_line
+execute_from_command_line(['manage.py', 'migrate', '--noinput'])
