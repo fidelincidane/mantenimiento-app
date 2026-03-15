cee0aed042308f1887e19ddfcd82c0ea5900263c
fidelincidane <fidelincidane@gmail.com>
2026-03-15 11:37:59 +0100

    Fix static files

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn mantenimiento.wsgi --log-file -
+web: python manage.py migrate && python manage.py collectstatic --noinput && gunicorn mantenimiento.wsgi --log-file -
--- mantenimiento/urls.py
+++ mantenimiento/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('preventivoapp.urls')),
 ]
 
-if settings.DEBUG:
+# Servir archivos estáticos en producción
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
