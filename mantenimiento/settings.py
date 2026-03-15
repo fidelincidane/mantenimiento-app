06c6ebbfcc500e9ad6e596fb4d38701b02dd02ba
fidelincidane <fidelincidane@gmail.com>
2026-03-15 13:08:22 +0100

    Fix static and media config

--- mantenimiento/settings.py
+++ mantenimiento/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Login URL
@@ -135,4 +135,4 @@ else:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- mantenimiento/urls.py
+++ mantenimiento/urls.py
@@ -2,17 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('preventivoapp.urls')),
 ]
 
-# Servir archivos media en producción
-if not settings.DEBUG:
-    urlpatterns += [
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-    ]
-else:
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
